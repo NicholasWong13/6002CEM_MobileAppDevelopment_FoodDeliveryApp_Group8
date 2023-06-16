61f6325b3afe465fda5082d8c3621c48bef93c1b
ranveersingh24 <103876712+ranveersingh24@users.noreply.github.com>
2023-06-16 22:03:03 +0800

    Add files via upload
    Upload colors.dart

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const rawButtoncolor = Color.fromARGB(167, 139, 130, 124);
+const operatorcolor = Color.fromARGB(255, 233, 155, 12);
+const buttoncolor = Color.fromARGB(192, 165, 153, 146);
